d036ee28b6153e1795d685de55cb389526053da9
Andreas Ågren <bandagr@gmail.com>
2020-11-06 22:14:41 +0100

    Added fake

---
+++ build.fsx
@@ -0,0 +1,30 @@
+#r "paket:
+nuget Fake.Core.Target
+nuget FSharp.Core 4.7
+//"
+// include Fake modules, see Fake modules section
+
+open Fake.Core
+
+// *** Define Targets ***
+Target.create "Clean" (fun _ ->
+  Trace.log " --- Cleaning stuff --- "
+)
+
+Target.create "Build" (fun _ ->
+  Trace.log " --- Building the app --- "
+)
+
+Target.create "Deploy" (fun _ ->
+  Trace.log " --- Deploying app --- "
+)
+
+open Fake.Core.TargetOperators
+
+// *** Define Dependencies ***
+"Clean"
+  ==> "Build"
+  ==> "Deploy"
+
+// *** Start Build ***
+Target.runOrDefault "Deploy"
